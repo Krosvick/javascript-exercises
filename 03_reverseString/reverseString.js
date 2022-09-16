6dc43cbe0400c86c97bc380552c8f2b681df513c
José <josesand99v@gmail.com>
2022-09-16 12:26:32 -0300

    Finish and pass all test on exercise 3 spliting str into an array to reverse it and then join

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str){
+    let arr=[];
+    arr = str.split("");
+    let revArr = [];
+    revArr = arr.reverse();
+    let revStr = revArr.join("");
+    return revStr;
 };
 
 // Do not edit below this line
