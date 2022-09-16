921cf5728fc1f42d718ed768770a2d11895ee2cf
José <josesand99v@gmail.com>
2022-09-16 13:11:48 -0300

    Finish and pass all test on the 5 exercise, using for loops

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(n, f) {
+    let sum=0;
+    if(n<f){
+        if(n<0 || f<0 || typeof n != "number" || typeof f != "number"){
+            return "ERROR";
+        }else{
+            for(let i=n; i<=f; i++){
+                sum+=i;
+            }
+            return sum;
+        } 
+    } else {
+        if(n<0 || f<0 || typeof n != "number" || typeof f != "number"){
+            return "ERROR";
+        }else{
+            for(let i=f; i<=n; i++){
+                sum+=i;
+            }
+            return sum;
+        } 
+    }
 
 };
 
